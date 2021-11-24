4b0703052fb0b463e90993ae0fc5d7d9c700361e
N6REJ <programming@hallhome.us>
2021-11-24 06:55:08 -0600

    updated secret

--- bin/phpmyadmin5.1.1/config.inc.php
+++ bin/phpmyadmin5.1.1/config.inc.php
@@ -14,7 +14,7 @@
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
  */
-$cfg['blowfish_secret'] = 'JnavIRZwZf9b2JYTlC8AYDhXo9HRLN1s'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'MJkN7gbnKdyEjaR0sY}C=aX/VUCZYH3T'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /*
  * Servers configuration
